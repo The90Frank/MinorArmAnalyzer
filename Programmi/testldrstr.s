e5b3b1a802d0e154c845dddab88b1f6888eec640
the90frank <francescov.90@gmail.com>
2019-11-14 16:12:14 +0100

    update test

--- Programmi/testldrstr.s
+++ Programmi/testldrstr.s
@@ -1,12 +1,164 @@
 	.section .text
 	.global _start
+	.global pippo
 
 _start:
-	mov r1, #42
-	mov r2, #16
-	str r1, [sp, r1]
-	ldr r2, [sp, r2]
+    mov r4, #42
+    mov r5, #4
+    mov r6, #33
+    nop
+
+    stm sp,{r0-r10}
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 	nop
-	mov r0, r2
-	mov r7, #1
-	SVC 0
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+pippo:
+    ldr r0,[sp,#0]
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    str r0,[sp,#0]
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+
+
+
+
+    mov r7, #1
+    SVC 0
