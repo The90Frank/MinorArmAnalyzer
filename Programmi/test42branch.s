02b97002c77865d473a9eae5d94cbeaede901ef5
The90Frank <francescov.90@gmail.com>
2026-02-19 17:44:21 +0100

    Set license to Apache-2.0

--- Programmi/test42branch.s
+++
@@ -1,8 +0,0 @@
-	.section .text
-	.global _start
-
-_start:
-	mov r0, #42
-	b _start
-	mov r7, #1
-	SVC 0
--- Programmi/testintdependency.s
+++
@@ -1,8 +0,0 @@
-	.section .text
-	.global _start
-
-_start:
-	mov r0, #21
-	add r0, r0, r0
-	mov r7, #1
-	SVC 0
